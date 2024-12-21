8aafdc1a95519177bed7dfc1cd9f79cdec6ab06d
Divya Salunke <divyasalunkhe697@gmail.com>
2024-12-21 22:54:22 +0530

    Add files via upload

---
+++ c micro project .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char city[100];
+    float temperature, humidity, wind_speed;
+
+    printf("Enter city name: ");
+    scanf("%s", city);
+
+    // Retrieve current weather conditions
+    temperature = 
+    humidity = 
+    wind_speed = 
+
+    printf("Weather in %s:\n", city);
+    printf("Temperature: %.2f°C\n", temperature);
+    printf("Humidity: %.2f%%\n", humidity);
+    printf("Wind Speed: %.2f m/s\n", wind_speed);
+
+    return 0;
+}
